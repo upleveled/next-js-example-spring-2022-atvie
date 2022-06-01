1210ac1aa1a60030795a58cc93c3bfd6e1986846
Lukas Prochazka <prochazka.lu@gmail.com>
2022-06-01 14:09:44 +0200

    Add test files

---
+++ util/__tests__/math.test.js
@@ -0,0 +1,13 @@
+import { sum } from '../math';
+
+test('adds two numbers together', () => {
+  expect(sum(1, 1)).toBe(2);
+  expect(sum(1, 2)).toBe(3);
+  expect(sum(-1, 1)).toBe(0);
+});
+
+test('throws an error if arguments are not numbers', () => {
+  expect(() => sum(1, '2')).toThrow('Arguments must be numbers');
+  expect(() => sum('1', 2)).toThrow('Arguments must be numbers');
+  expect(() => sum('hallo', '2')).toThrow('Arguments must be numbers');
+});
