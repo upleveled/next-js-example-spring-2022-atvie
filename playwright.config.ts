83d27c6fcd36df15e3710aa0c92544bcae9ffdff
Lukas Prochazka <prochazka.lu@gmail.com>
2022-06-03 12:23:24 +0200

    Add Playwright yml file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,12 @@
 import { devices, PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  webServer: {
+    command: 'yarn start',
+    port: 3000,
+    timeout: 10 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
