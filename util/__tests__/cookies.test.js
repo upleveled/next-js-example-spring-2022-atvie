dd79c9adedee1a57de3ad6d3897f08b762691ecc
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-06-03 13:23:17 +0200

    Fix jest conflicts

--- util/__tests__/cookies.test.js
+++ util/__tests__/cookies.test.js
@@ -5,35 +5,6 @@ import {
   stringifyCookieValue,
 } from '../cookies';
 
-<<<<<<< HEAD
-// This is closest to what you want your unit
-// tests to be like - testing a single, small
-// function that is pure (doesn't have any
-// side effects)
-test('stringifies a cookie value', () => {
-  expect(stringifyCookieValue({ 1: 10, 2: 15 })).toBe(
-    JSON.stringify({ 1: 10, 2: 15 }),
-  );
-});
-
-// Also tests the implementation details of the
-// js-cookie library, which may not be as useful
-// for you
-test('sets, gets and deletes a cookie', () => {
-  const cookie = {
-    key: 'stars',
-    value: { 1: 3, 2: 4 },
-  };
-  // First, make sure that the value at the start is undefined
-  expect(getParsedCookie(cookie.key)).toBe(undefined);
-
-  // Set the cookie value and test that the value was updated
-  expect(setStringifiedCookie(cookie.key, cookie.value)).toBeUndefined();
-  // expect(setStringifiedCookie(cookie.key, cookie.value)).toBe(undefined);
-  expect(getParsedCookie(cookie.key)).toStrictEqual(cookie.value);
-
-  // Best practice: tests clean up any state after themselves
-=======
 // This is closest to what we want in unit tests
 // testing a single, small function that is pure
 test('stringify a cookie value', () => {
@@ -54,7 +25,6 @@ test('set, gets and delete a cookie', () => {
   // expect(setStringifiedCookie(cookie.key, cookie.value)).toBeUndefined();
   expect(getParsedCookie(cookie.key)).toStrictEqual(cookie.value);
   // Best practice: clean up after test
->>>>>>> main
   expect(deleteCookie(cookie.key)).toBe(undefined);
   expect(getParsedCookie(cookie.key)).toBe(undefined);
 });
--- util/__tests__/math.test.js
+++ util/__tests__/math.test.js
@@ -1,26 +1,14 @@
 import { sum } from '../math';
 
-<<<<<<< HEAD
-test('adds two numbers together', () => {
-  expect(sum(1, 1)).toBe(2);
-  expect(sum(1, 2)).toBe(3);
-  expect(sum(-1, 1)).toBe(0);
-=======
 test('add two numbers together', () => {
   expect(sum(1, 1)).toBe(2);
   expect(sum(-100, 200)).toBe(100);
   expect(sum(1, 1)).not.toBe(100);
->>>>>>> main
 });
 
 test('throws an error if arguments are not numbers', () => {
   expect(() => sum(1, '2')).toThrow('Pass only numbers!');
-<<<<<<< HEAD
-  expect(() => sum('1', 2)).toThrow('Pass only numbers!');
-  expect(() => sum('hallo', '2')).toThrow('Pass only numbers!');
-=======
   expect(() => sum('1', 'asdasd')).toThrow('Pass only numbers!');
   // expect(() => sum(1, 1)).toThrow('Pass only numbers!');
   // expect(() => sum(NaN, 1)).toThrow('Pass only numbers!');
->>>>>>> main
 });
