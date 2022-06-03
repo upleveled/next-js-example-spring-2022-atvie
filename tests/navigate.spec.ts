0ee457509666d8c4fb644d0330111a4d725e87d9
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-06-03 14:32:22 +0200

    Add await to tests

--- tests/navigate.spec.ts
+++ tests/navigate.spec.ts
@@ -4,26 +4,28 @@ const baseUrl = 'http://localhost:3000/';
 
 test('navigation test', async ({ page }) => {
   await page.goto(baseUrl);
-  const titleLocator = page.locator('h1');
+  const titleLocator = await page.locator('h1');
   await expect(titleLocator).toHaveText('Tierpark');
 
-  // await Promise.all([
   await page.locator('text=Fruits').click();
   await page.waitForNavigation({ url: `${baseUrl}fruits` });
-  // ]);
+
   await expect(titleLocator).toHaveText('Fruits');
   const fruitsList = await page.$$('[data-test-id^="fruits-page-fruit-"]');
-  expect(fruitsList.length).toBe(4);
-  const fruitsLocator = page.locator('[data-test-id^="fruits-page-fruit-"]');
+  await expect(fruitsList.length).toBe(4);
+  const fruitsLocator = await page.locator(
+    '[data-test-id^="fruits-page-fruit-"]',
+  );
   await expect(fruitsLocator).toHaveText([
     'papaya',
     'apple',
     'lemon',
     'banana',
   ]);
-  await expect(page.locator('[data-test-id="fruits-page-fruit-4"]')).toHaveText(
-    'banana',
-  );
+  await expect(
+    await page.locator('[data-test-id="fruits-page-fruit-4"]'),
+  ).toHaveText('banana');
+
   await page.locator('text=banana').click();
   await page.waitForNavigation({ url: `${baseUrl}fruits/4` });
   await page.locator('text=add to diet').click();
