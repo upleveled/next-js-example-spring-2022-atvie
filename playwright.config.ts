60f0610282179af6e32a17bf8e2459c1f012a671
Lukas Prochazka <prochazka.lu@gmail.com>
2022-06-07 15:12:02 +0200

    Delete unnecessary comments/simplify test config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,48 +1,28 @@
 import { devices, PlaywrightTestConfig } from '@playwright/test';
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 const config: PlaywrightTestConfig = {
-  /* Run your local dev server before starting the tests */
+  // Runs your local server before starting the tests,
+  // so you don't have to run your dev server manually.
   webServer: {
     command: 'yarn start',
     port: 3000,
-    timeout: 10 * 1000,
     reuseExistingServer: !process.env.CI,
   },
-  /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
-  expect: {
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
-    timeout: 5000,
-  },
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Ignore JEST test files */
+  // Ignore Jest test files
   testIgnore: '**/util/__tests__/**',
-  /* Retry on CI only */
+  // Retry on CI only, because if the Github Actions test fails,
+  // it will retry the whole test suite.
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  // Setup reporter for Github Actions that prints a line for each test being run.
+  // Locally HTML reporter produces a self-contained folder that
+  // contains report for the test run that can be served as a web page.
   reporter: process.env.CI ? 'list' : 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
@@ -51,8 +31,7 @@ const config: PlaywrightTestConfig = {
       },
     },
   ],
-
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  // Creates folder for test artifacts such as screenshots, videos, traces, etc.
   outputDir: 'test-results/',
 };
 
