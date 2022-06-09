0a55cec3351a51424480f3baf0461d238a1addc4
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-06-09 11:48:05 +0200

    Setup project to be deployed

---
+++ util/setPostgresDefaultsOnHeroku.js
@@ -0,0 +1,15 @@
+module.exports = function setPostgresDefaultsOnHeroku() {
+  if (process.env.DATABASE_URL) {
+    // Install pg-connection-string
+    const { parse } = require('pg-connection-string');
+
+    // Extract the connection information from the Heroku environment variable
+    const { host, database, user, password } = parse(process.env.DATABASE_URL);
+
+    // Set standard environment variables
+    process.env.PGHOST = host;
+    process.env.PGDATABASE = database;
+    process.env.PGUSERNAME = user;
+    process.env.PGPASSWORD = password;
+  }
+};
