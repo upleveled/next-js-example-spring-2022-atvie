c94a54f6ddc5c35ea6820059a4bdb3ba4ac58ef3
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-09-21 17:08:19 +0200

    Update files for fly.io deploy

---
+++ postgres-init.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+if [ ! -d "../postgres-volume" ]
+then
+  mkdir -p /run/postgresql/data/
+  chown postgres:postgres /run/postgresql/data /run/postgresql/
+  su postgres -c "initdb -D /run/postgresql/data"
+  echo "host all all 0.0.0.0/0 trust" >> /run/postgresql/data/pg_hba.conf
+  echo "listen_addresses='*'" >> /run/postgresql/data/postgresql.conf
+  su postgres -c "pg_ctl start -D /run/postgresql/data"
+  psql -U postgres postgres --command="CREATE USER $PGUSERNAME PASSWORD '$PGPASSWORD'"
+  createdb -U postgres --owner=$PGUSERNAME $PGDATABASE
+else
+  mkdir /postgres-volume/data /postgres-volume/run
+  chown postgres:postgres /postgres-volume/data /postgres-volume/run
+  [ ! -f /postgres-volume/data/postgresql.conf ] && su postgres -c "initdb -D /postgres-volume/data"
+  sed -i 's/run\/postgresql/postgres-volume\/run/g' /postgres-volume/data/postgresql.conf
+  grep -qxF "listen_addresses='*'" /postgres-volume/data/postgresql.conf || echo "listen_addresses='*'" >> /postgres-volume/data/postgresql.conf
+  [ ! -f /postgres-volume/data/postmaster.pid ] && su postgres -c "pg_ctl start -D /postgres-volume/data" || su postgres -c "pg_ctl restart -D /postgres-volume/data"
+  psql -U postgres postgres --command="CREATE USER $PGUSERNAME PASSWORD '$PGPASSWORD'" || echo "User already exists"
+  createdb -U postgres --owner=$PGUSERNAME $PGDATABASE || echo "Database already exists"
+fi
--- util/database.ts
+++ util/database.ts
@@ -1,11 +1,5 @@
 import camelcaseKeys from 'camelcase-keys';
-import { config } from 'dotenv-safe';
 import postgres from 'postgres';
-import setPostgresDefaultsOnHeroku from './setPostgresDefaultsOnHeroku';
-
-setPostgresDefaultsOnHeroku();
-
-config();
 
 // Type needed for the connection function below
 declare module globalThis {
@@ -15,19 +9,10 @@ declare module globalThis {
 // Connect only once to the database
 // https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
 function connectOneTimeToDatabase() {
-  let sql;
-
-  if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL) {
-    // Heroku needs SSL connections but
-    // has an "unauthorized" certificate
-    // https://devcenter.heroku.com/changelog-items/852
-    sql = postgres({ ssl: { rejectUnauthorized: false } });
-  } else {
-    if (!globalThis.postgresSqlClient) {
-      globalThis.postgresSqlClient = postgres();
-    }
-    sql = globalThis.postgresSqlClient;
+  if (!globalThis.postgresSqlClient) {
+    globalThis.postgresSqlClient = postgres();
   }
+  const sql = globalThis.postgresSqlClient;
 
   return sql;
 }
