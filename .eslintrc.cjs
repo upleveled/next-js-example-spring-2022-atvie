da99310b667c7c15450a5132950b5006ef0008d3
Victor Ejike Nwosu <ejikello123@yahoo.com>
2022-09-27 14:02:35 +0200

    Add SafeQL configuration settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,33 @@
+require('dotenv-safe').config({
+  path: './.env',
+});
+
 module.exports = {
   extends: ['@upleveled/upleveled'],
+
+  plugins: ['@ts-safeql/eslint-plugin'],
+
+  rules: {
+    '@ts-safeql/check-sql': [
+      'error',
+      {
+        connections: [
+          {
+            // The URL of the database:
+            // databaseUrl:
+            databaseUrl: `postgres://${process.env.PGUSERNAME}:${process.env.PGPASSWORD}@${process.env.PGHOST}:5432/${process.env.PGDATABASE}`,
+
+            // The name of the variable that holds the connection:
+            tagName: 'sql',
+
+            // Transform data
+            fieldTransform: 'camel',
+
+            // Postgres.js type should be an array, so we add an extra "[]" after the generated type:
+            transform: '${type}[]',
+          },
+        ],
+      },
+    ],
+  },
 };
