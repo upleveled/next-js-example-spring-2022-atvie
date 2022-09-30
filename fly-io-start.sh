7e4bfaac507a146f8b6437fd061e4c97d861d9e4
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-09-30 16:56:01 +0200

    Use variables to reduce code repetition

--- fly-io-start.sh
+++ fly-io-start.sh
@@ -6,25 +6,17 @@ then
   exit 0
 fi
 
-if [ ! -d "../postgres-volume" ]
-then
-  mkdir -p /run/postgresql/data/
-  chown postgres:postgres /run/postgresql/data /run/postgresql/
-  su postgres -c "initdb -D /run/postgresql/data"
-  echo "listen_addresses='*'" >> /run/postgresql/data/postgresql.conf
-  su postgres -c "pg_ctl start -D /run/postgresql/data"
-  psql -U postgres postgres --command="CREATE USER $PGUSERNAME PASSWORD '$PGPASSWORD'"
-  createdb -U postgres --owner=$PGUSERNAME $PGDATABASE
-else
-  mkdir -p /postgres-volume/run/postgresql/data/
-  chown postgres:postgres /postgres-volume/run/postgresql/data /postgres-volume/run/postgresql/
-  [ ! -f  /postgres-volume/run/postgresql/data/postgresql.conf ] && su postgres -c "initdb -D /postgres-volume/run/postgresql/data"
-  sed -i 's/run\/postgresql/postgres-volume\/run\/postgresql/g'  /postgres-volume/run/postgresql/data/postgresql.conf
-  grep -qxF "listen_addresses='*'"  /postgres-volume/run/postgresql/data/postgresql.conf || echo   "listen_addresses='*'" >>  /postgres-volume/run/postgresql/data/postgresql.conf
-  [ ! -f  /postgres-volume/run/postgresql/data/postmaster.pid ] && su postgres -c "pg_ctl start -D /postgres-volume/run/postgresql/data" || su postgres -c "pg_ctl restart -D  /postgres-volume/run/postgresql/data"
-  psql -U postgres postgres --command="CREATE USER $PGUSERNAME PASSWORD'$PGPASSWORD'" ||    echo "User already exists"
-  createdb -U postgres --owner=$PGUSERNAME $PGDATABASE || echo "Database already exists"
-fi
+# A
+[ -d "../postgres-volume" ] && VOLUME_PATH=/postgres-volume
 
+mkdir -p $VOLUME_PATH/run/postgresql/data/
+chown postgres:postgres $VOLUME_PATH/run/postgresql/data/ $VOLUME_PATH/run/postgresql/
+
+[ ! -f $VOLUME_PATH/run/postgresql/data/postgresql.conf ] && su postgres -c "initdb -D $VOLUME_PATH/run/postgresql/data/"
+sed -i "s/'\/run\/postgresql\'/'\/postgres-volume\/run\/postgresql'/g" /postgres-volume/run/postgresql/data/postgresql.conf || echo "postgres-volumenot mounted"
+grep -qxF "listen_addresses='*'"  $VOLUME_PATH/run/postgresql/data/postgresql.conf || echo "listen_addresses='*'" >>  $VOLUME_PATH/run/postgresql/datapostgresql.conf
+su postgres -c "pg_ctl restart -D /postgres-volume/run/postgresql/data/" || su postgres -c "pg_ctl start -D $VOLUME_PATH/run/postgresql/data/"
+psql -U postgres postgres --command="CREATE USER $PGUSERNAME PASSWORD '$PGPASSWORD'" || echo "User already exists"
+createdb -U postgres --owner=$PGUSERNAME $PGDATABASE || echo "Database already exists"
 yarn migrate up
 yarn start
