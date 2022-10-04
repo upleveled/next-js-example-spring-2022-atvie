49c497dbb90ef07648589c3edae556cab6885a34
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-10-04 14:17:33 +0200

    Update script to initialize database conditionaly

--- fly-io-start.sh
+++ fly-io-start.sh
@@ -3,12 +3,15 @@
 # Add Postgres volume path when it exist
 [ -d "../postgres-volume" ] && VOLUME_PATH=/postgres-volume
 
+# Create ENV variable when database need to be initialized
+[ ! -f $VOLUME_PATH/run/postgresql/data/postgresql.conf ] && DATABASE_INIT=1
+
 # Create and add permissions to folders for PostgreSQL
 mkdir -p $VOLUME_PATH/run/postgresql/data/
 chown postgres:postgres $VOLUME_PATH/run/postgresql/ $VOLUME_PATH/run/postgresql/data/
 
-# Initialize and configure the database
-[ ! -f $VOLUME_PATH/run/postgresql/data/postgresql.conf ] && su postgres -c "initdb -D $VOLUME_PATH/run/postgresql/data/"
+# Initialize database when needed
+[ $DATABASE_INIT == 1 ] && su postgres -c "initdb -D $VOLUME_PATH/run/postgresql/data/"
 sed -i "s/'\/run\/postgresql\'/'\/postgres-volume\/run\/postgresql'/g" /postgres-volume/run/postgresql/data/postgresql.conf || echo "Postgres volume not mounted"
 grep -qxF "listen_addresses='*'"  $VOLUME_PATH/run/postgresql/data/postgresql.conf || echo "listen_addresses='*'" >>  $VOLUME_PATH/run/postgresql/datapostgresql.conf
 
@@ -16,8 +19,8 @@ grep -qxF "listen_addresses='*'"  $VOLUME_PATH/run/postgresql/data/postgresql.co
 su postgres -c "pg_ctl restart -D /postgres-volume/run/postgresql/data/" || su postgres -c "pg_ctl start -D $VOLUME_PATH/run/postgresql/data/"
 
 # Use the credentials from Fly.io secrets to create user and database when needed
-psql -U postgres postgres --command="CREATE USER $PGUSERNAME PASSWORD '$PGPASSWORD'" || echo "User already exists"
-createdb -U postgres --owner=$PGUSERNAME $PGDATABASE || echo "Database already exists"
+[ $DATABASE_INIT == 1 ] && psql -U postgres postgres --command="CREATE USER $PGUSERNAME PASSWORD '$PGPASSWORD'"
+[ $DATABASE_INIT == 1 ] && createdb -U postgres --owner=$PGUSERNAME $PGDATABASE
 
 # Run migrations and start the production server
 node /app/node_modules/ley/bin.js up
