bb0715ae604d52bcd42e2176a121ba7508f3c260
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-10-06 15:05:54 +0200

    Update comments to improve clarity

--- scripts/fly-io-start.sh
+++ scripts/fly-io-start.sh
@@ -10,23 +10,25 @@ set -e
 mkdir -p $VOLUME_PATH/run/postgresql/data/
 chown postgres:postgres $VOLUME_PATH/run/postgresql/ $VOLUME_PATH/run/postgresql/data/
 
-# Initialize database only when database config file doesn't exist yet, this happens during:
-# 1. Deployment / redeployment of an app without a volume
-# 2. First deployment of an app with a volume
+# If PostgreSQL config file exists, restart database. Otherwise, initialize, configure and create user and database.
+#
+# Config file doesn't exist during:
+# 1. First deployment of an app with a volume
+# 2. Every deployment of an app without a volume
 if [[ -f $VOLUME_PATH/run/postgresql/data/postgresql.conf ]]; then
-  # Restart database after app with a volume is redeployed
+  # Restart database
   su postgres -c "pg_ctl restart -D /postgres-volume/run/postgresql/data/"
 else
   # Initialize a database in the data directory
   su postgres -c "initdb -D $VOLUME_PATH/run/postgresql/data/"
 
   # Update PostgreSQL config path to use volume location if app has a volume
-  sed -i "s/'\/run\/postgresql'/'\/postgres-volume\/run\/postgresql'/g" /postgres-volume/run/postgresql/data/  postgresql.conf || echo "PostgreSQL volume not mounted, running database as non-persistent (new deploys erase   changes not saved in migrations)"
+  sed -i "s/'\/run\/postgresql'/'\/postgres-volume\/run\/postgresql'/g" /postgres-volume/run/postgresql/data/postgresql.conf || echo "PostgreSQL volume not mounted, running database as non-persistent (new deploys erase changes not saved in migrations)"
 
-  # Configure PostgreSQL to listen requests by adding a configuration string only once
-  echo "listen_addresses='*'" >> $VOLUME_PATH/run/postgresql/datapostgresql.conf
+  # Configure PostgreSQL to listen for connections from any address
+  echo "listen_addresses='*'" >> $VOLUME_PATH/run/postgresql/data/postgresql.conf
 
-  # Start database after database is created
+  # Start database
   su postgres -c "pg_ctl start -D $VOLUME_PATH/run/postgresql/data/"
 
   # Create user and database with credentials from Fly.io secrets
