af928dfe8ee395b79db6ce9dc4d83b12eb341166
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-10-06 10:21:39 +0200

    Remove extra spaces in script

--- scripts/fly-io-start.sh
+++ scripts/fly-io-start.sh
@@ -24,7 +24,7 @@ su postgres -c "initdb -D $VOLUME_PATH/run/postgresql/data/"
 sed -i "s/'\/run\/postgresql'/'\/postgres-volume\/run\/postgresql'/g" /postgres-volume/run/postgresql/data/postgresql.conf || echo "PostgreSQL volume not mounted, running database as non-persistent (new deploys erase changes not saved in migrations)"
 
 # Configure PostgreSQL to listen requests by adding a configuration string only once
-grep -qxF "listen_addresses='*'"  $VOLUME_PATH/run/postgresql/data/postgresql.conf || echo "listen_addresses='*'" >>  $VOLUME_PATH/run/postgresql/datapostgresql.conf
+grep -qxF "listen_addresses='*'" $VOLUME_PATH/run/postgresql/data/postgresql.conf || echo "listen_addresses='*'" >> $VOLUME_PATH/run/postgresql/datapostgresql.conf
 
 # Start database after database is created
 su postgres -c "pg_ctl start -D $VOLUME_PATH/run/postgresql/data/"
